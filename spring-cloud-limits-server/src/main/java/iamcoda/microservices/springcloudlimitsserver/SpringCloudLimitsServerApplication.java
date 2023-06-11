60e23f15213d1d039ee379b31d5416421ce2d829
Amy <ampyaephyonaing@gmail.com>
2023-06-11 15:53:29 +0800

    Creating config repository

---
+++ spring-cloud-limits-server/src/main/java/iamcoda/microservices/springcloudlimitsserver/SpringCloudLimitsServerApplication.java
@@ -0,0 +1,13 @@
+package iamcoda.microservices.springcloudlimitsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLimitsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLimitsServerApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-limits-server/src/test/java/iamcoda/microservices/springcloudlimitsserver/SpringCloudLimitsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package iamcoda.microservices.springcloudlimitsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLimitsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
