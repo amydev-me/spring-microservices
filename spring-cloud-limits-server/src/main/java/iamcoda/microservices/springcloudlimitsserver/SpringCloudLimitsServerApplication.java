0e308a95866f8a67a055dad4ffc1a77d29e04096
Amy <ampyaephyonaing@gmail.com>
2023-06-11 16:29:29 +0800

    Add git config

--- spring-cloud-limits-server/src/main/java/iamcoda/microservices/springcloudlimitsserver/SpringCloudLimitsServerApplication.java
+++ spring-cloud-limits-server/src/main/java/iamcoda/microservices/springcloudlimitsserver/SpringCloudLimitsServerApplication.java
@@ -2,7 +2,9 @@ package iamcoda.microservices.springcloudlimitsserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudLimitsServerApplication {
 
