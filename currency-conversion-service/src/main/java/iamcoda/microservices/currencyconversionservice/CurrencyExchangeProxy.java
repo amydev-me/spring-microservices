831d4b5b75f536d8812aeaf8b511481bb4600dfc
Amy <ampyaephyonaing@gmail.com>
2023-06-12 23:19:17 +0800

    Currency Conversion Service with Feign

---
+++ currency-conversion-service/src/main/java/iamcoda/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,13 @@
+package iamcoda.microservices.currencyconversionservice;
+
+import iamcoda.microservices.currencyconversionservice.CurrencyConversion.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="currency-exchange", url = "http://localhost:8000")
+public interface CurrencyExchangeProxy {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable String from,
+                                                    @PathVariable String to);
+}
